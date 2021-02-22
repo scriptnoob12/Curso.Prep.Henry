6d61e3d622795956dbbffdff1cdba60e61bbf1db
scriptnoob12 <scriptnoob12@gmail.com>
2021-02-22 13:15:55 -0300

    Homework 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,26 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let nombre2="";
+  nombre.split("").forEach(function(letra,i){
+    if(i===0){nombre2=nombre2.concat(letra.toUpperCase());}else {nombre2=nombre2.concat(letra);}
+   
+  });
+  return nombre2;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,25 +31,47 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  return cb(numeros.reduce(function(val,suma){
+    return val+suma;
+  }));
+  
+
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(val){
+    return cb(val);
+  });
 }
-
+// Quizás este mal, pero intenté adaptarlo al test
 function map(array, cb) {
   // Crea un nuevo array
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var array2=array.map(function(el){
+    return cb(el);
+  });
+  return array2;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  let arr=[];
+  function cb(val){
+    return arr.push(val);
+  }
+  array.map(function(val){
+    if(val[0]=== "a"){
+      return cb(val);
+    };
+  });
+  return arr;
 }
 
 // No modificar nada debajo de esta línea
